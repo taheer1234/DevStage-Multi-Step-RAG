e4421d65b48d63e253a7f7c2c02b61c442f42970
Taheer Aamir <taheeraamir6282@gmail.com>
2025-08-22 12:24:50 +0500

    added another retriever

--- app_modules/retrievers.py
+++ app_modules/retrievers.py
@@ -17,11 +17,23 @@ print("Initialized Embedding Model!")
 pc = Pinecone()
 
 
-def pinecone_mmr_retriever(index_name: str):
+def pinecone_mmr_retriever(index_name: str, k: int, lambda_mult: float):
     """
     This function returns a retriver that uses the
-    Pinecone Vector Store and mmr for searching the Vector Store.
+    Pinecone Vector Store and the mmr search type for searching the Vector Store.
     """
     index = pc.Index(index_name)
     vector_store = PineconeVectorStore(index=index, embedding=embedding_model)
-    return vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 10})
+    return vector_store.as_retriever(
+        search_type="mmr", search_kwargs={"k": k, "lambda_mult": lambda_mult}
+    )
+
+
+def pinecone_similarity_retriever(index_name: str, k: int):
+    """
+    This function returns a retriver that uses the
+    Pinecone Vector Store and the similarity search type for searching the Vector Store.
+    """
+    index = pc.Index(index_name)
+    vector_store = PineconeVectorStore(index=index, embedding=embedding_model)
+    return vector_store.as_retriever(search_type="similarity", search_kwargs={"k": k})

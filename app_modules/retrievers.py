ab4e3988ff455d71f277211dc376b987a23fb9fa
Taheer Aamir <taheeraamir6282@gmail.com>
2025-08-22 12:18:22 +0500

    updated retrievers

--- app_modules/retrievers.py
+++ app_modules/retrievers.py
@@ -1,16 +1,27 @@
 """
 This module contains the Retriever(s) that could be used during the Multi-Step RAG's execution.
 """
+
 import os
 from pinecone.pinecone import Pinecone
 from langchain_pinecone import PineconeVectorStore
 from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
+
 load_dotenv()
 
+print("Initializing Embedding Model...")
 embedding_model = HuggingFaceEmbeddings(model_name=os.environ["EMBEDDING_MODEL_NAME"])
+print("Initialized Embedding Model!")
+
+pc = Pinecone()
 
 
-def pinecone_mmr_retriever():
-    pass
-
+def pinecone_mmr_retriever(index_name: str):
+    """
+    This function returns a retriver that uses the
+    Pinecone Vector Store and mmr for searching the Vector Store.
+    """
+    index = pc.Index(index_name)
+    vector_store = PineconeVectorStore(index=index, embedding=embedding_model)
+    return vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 10})

6b787a2c7a4a9a8e275e18236f80cfee1f5b41b9
Taheer Aamir <taheeraamir6282@gmail.com>
2025-08-26 20:53:25 +0500

    reconfigured import to new path for graph in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """This script runs the Multi-Step RAG."""
 
-from app_modules.graph import MultiStepRAGGraphBuilder
+from app_modules.graph_config.graph import MultiStepRAGGraphBuilder
 
 graph_builder = MultiStepRAGGraphBuilder()
 graph = graph_builder.create_graph()
